267a599ec4adb9a50a08f9dfad6c1722647f281b
Efrem Lapin <efremlapin@vk.com>
2022-11-09 19:20:28 +0300

    remove decoding images

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -67,7 +67,6 @@ class UserController {
         if (passwordRes) {
           const { id, email } = data[0];
           const token = generateToken(id, email);
-          data[0].avatar = Buffer.from(data[0].avatar).toString('base64');
           res.json({token, ...data[0]});
         }
       }
@@ -81,7 +80,6 @@ class UserController {
         res.json(err);
         console.log(err);
       } else {
-        data[0].avatar = Buffer.from(data[0].avatar).toString('base64');
         res.json(data);
       }
     });
